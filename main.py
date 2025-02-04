e016c202d32bc186d284693824a022b37966d0a9
alvarojpr <alvarojosedm9@gmail.com>
2025-02-04 12:23:48 -0300

    segunda ação

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello":"WOOOOOOOOOOORLD!"}
+    return {"Hello":"World!"}
 
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
