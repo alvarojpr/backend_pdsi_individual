1d67c2e19d1398c2f55f671e4e1f6ecbb98f6569
alvarojpr <alvarojosedm9@gmail.com>
2025-02-04 12:08:08 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, status, Depends
+import classes, model
+from database import engine, get_db
+from sqlalchemy.orm import Session
+
+model.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
+
+@app.post("/criar", status_code=status.HTTP_201_CREATED)
+def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
+    # mensagem_criada = model.Model_Mensagem(titulo=nova_mensagem.tutulo,conteudo=nova_mensagem.conteudo, publicada=nova_mensagem.publicada)
+    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump)
+    db.add(mensagem_criada)
+    db.commit()
+    db.refresh(mensagem_criada)
+    return{"Mensagem:": mensagem_criada}
+
+    # print(nova_mensagem)
+    # return {"Mensagem": f"Titulo: {nova_mensagem.titulo} Conteudo: {nova_mensagem.conteudo} Publicada: {nova_mensagem.publicada}"}
+
+
+@app.get("/")
+def square(num: int):
+    return num ** 2
