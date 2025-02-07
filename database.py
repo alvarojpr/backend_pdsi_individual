41b31178767e44928f55fc77b853c08b1b2e3691
alvarojpr <alvarojosedm9@gmail.com>
2025-02-07 14:56:02 -0300

    feat webscrapping

--- database.py
+++ database.py
@@ -1,21 +1,32 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv # biblioteca para carregar variáveis de ambiente
 
-user = "postgres"
-password = "password"
-database = "nome_database"
-host = "localhost"
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env') # caminho para o arquivo .env: 
+load_dotenv(dotenv_path) # carrega as variáveis de ambiente
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL") # pega a variável de ambiente DATABASE_URL
 
-SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
-Base = declarative_base()
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não foi encontrada no arquivo .env")
 
+engine = create_engine(DATABASE_URL) # cria o engine que é responsável por se conectar ao banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # sessão, responsável por fazer as queries no banco de dados
+Base = declarative_base() # cria a base que é usada para criar as tabelas no banco de dados
+
+#função que retorna a sessão para as rotas manipularem o banco de dados
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+        
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, status, Depends
 import classes, model
 from database import engine, get_db
 from sqlalchemy.orm import Session
+import requests
+from bs4 import BeautifulSoup
+
 
 model.Base.metadata.create_all(bind=engine)
 
@@ -14,7 +17,7 @@ def read_root():
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
     # mensagem_criada = model.Model_Mensagem(titulo=nova_mensagem.tutulo,conteudo=nova_mensagem.conteudo, publicada=nova_mensagem.publicada)
-    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump)
+    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
     db.add(mensagem_criada)
     db.commit()
     db.refresh(mensagem_criada)
@@ -23,7 +26,49 @@ def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)
     # print(nova_mensagem)
     # return {"Mensagem": f"Titulo: {nova_mensagem.titulo} Conteudo: {nova_mensagem.conteudo} Publicada: {nova_mensagem.publicada}"}
 
-
 @app.get("/quadrado/{num}")
 def square(num: int):
     return num ** 3
+
+@app.post("/webscrapping")
+def webScrapping(db: Session = Depends(get_db)):
+    response = requests.get('https://ufu.br')
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Encontra a navegação desejada
+    nav_section = soup.find('nav', id='block-ufu-rodape-2')
+    
+    if nav_section:
+        list_items = nav_section.find_all('li', class_='nav-item')
+
+        for li in list_items:
+            a_tag = li.find('a', class_='nav-link')
+            if a_tag:
+                menu_nav = a_tag.text.strip()
+                link = 'https://ufu.br' + a_tag['href']
+
+                # Cria um objeto SQLAlchemy e adiciona ao banco
+                novo_menu = model.Model_Desafio(menuNav=menu_nav, link=link)
+                db.add(novo_menu)
+
+        # Confirma as inserções
+        db.commit()
+        print("Dados inseridos com sucesso!")
+
+    else:
+        print("Seção de navegação não encontrada.")
+
+# uvicorn app.main:app --reload --root-path server
+
+# ATUALIZAR
+# git fetch origin
+# git pull origin main
+
+# SUBIR MUDANÇAS
+# git add .                 git add requirements.txt
+# git commit -m "mensagem"
+# git push origin main
+
+# GERAR E INSATALAR requirements.txt
+# pip freeze > requirements.txt
+# pip install requirements.txt
